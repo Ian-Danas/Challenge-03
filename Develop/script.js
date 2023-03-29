b2e9db519e115a067ae7f1051fcf0309bef179cb
Ian Danas <ianmdanas@gmail.com>
2023-03-29 15:58:23 -0700

    adding logic to choose letters from acceptable character strings, and adding README content

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,53 @@
 // Assignment Code
 function generatePassword(){
-  specialChar = confirm("would you like it to have special characters?")
-  capitalLetters = confirm("Would you like the password to have capital letters")
-  Numbers = confirm("would you like it to have special characters?")
+  var strCapitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var strLowercase = 'abcdefghijklmnopqrstuvwxyz'
+  var strNums = '1234567890'
+  var strSpecialchars = '~!@#$%^&*()_+`=,./<>?;:'
+  var toPullfrom =''
 
-  if (!specialChar && !capitalLetters && !Numbers){
+  var lowerCaseinput = confirm("Would you like the password to have lowercase letters")
+  var specialCharinput = confirm("would you like it to have special characters?")
+  var capitalLettersinput = confirm("Would you like the password to have capital letters")
+  var numbersInputs = confirm("would you like it to have numbers?")
+
+  if (!specialCharinput && !capitalLettersinput && !numbersInputs && !lowerCaseinput){
     alert("Password must have at least one of these")
     return;
   }else{
-    numChar = prompt("How many characters would you like?")
+    var numCharinput = prompt("How many characters would you like?")
+    console.log(typeof numCharinput)
   }
-  if ((numChar < 8) ){
+  if(isNaN(numCharinput)){
+    alert('Input needs to be a number')
+
+  }else if ((numCharinput < 8) ){
     alert("That is too little characters. Password must be at least 8 characters and no more than 128 characters")
     return;
-  }else if (numChar > 128){
+  }else if (numCharinput > 128){
      alert("That is too many characters. Password must be at least 8 characters and no more than 128 characters")
     return;
   }
-  return 'testing'
 
-
+  if (lowerCaseinput){
+    toPullfrom = toPullfrom + strLowercase
+  }
+  if (capitalLettersinput){
+    toPullfrom = toPullfrom + strCapitals
+  }
+  if (specialCharinput){
+    toPullfrom = toPullfrom + strSpecialchars
+  }
+  if (numbersInputs){
+    toPullfrom = toPullfrom + strNums
+  }
+  var finalPassword = ''
+  for (let i = 0; i < numCharinput; i++) {
+     finalPassword = finalPassword + toPullfrom[Math.floor(Math.random() * toPullfrom.length)];
+    
+  }
+  return finalPassword
 }
-
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Challenge-03
+## Description
+
+This project provides a website for creating a random password following certain criteria given by the user. The user is able to put in if they want uppercase and or lowercase letters, numbers, or special characters. As well as how many characters they would like the password to be. The generator will then display a password on the website meeting those criteria.
+
+## Installation
+
+N/A
+
+## Usage
+To use this website start by clicking on the generate password button. Several prompts will popup asking if you would like uppercase, lowercase, numbers, or special characters. If you would like those included in your password select confirm on those prompts. If no options are selected an alert will popup saying that you must select at least one those and the process will need to be restarted. After all critera prompts are choosen it will ask how many characters you would like. You must enter a number between 8 and 128 inclusive. If the incorrect number of characters or an input that is not a character is entered you will be prompted to restart the process. When all criteria and length requirements are met your generated password will be generated in the box above the generate password button. 
+
+## Credits
+N/A
+
+## License
+N/A
